12367ef87ad19d7b7db6caa2fdd928f6c5323ae1
Selim Elkaffas <selimkaffas@gmail.com>
2026-04-05 17:51:30 +0200

    Implemented sync settings

--- src/renderer/src/SyncSettings.jsx
+++ src/renderer/src/SyncSettings.jsx
@@ -1,8 +1,92 @@
-
+import { useLiveQuery } from "dexie-react-hooks"
+import { useState } from "react"
+import { db } from "./db/db"
+import { pb } from "./db/pocketbase"
 
 function SyncSettings() {
+    const unsyncedPatients = useLiveQuery(() => db.patients.where('synced').equals(0).toArray(), []) || []
+    const unsyncedSnapshots = useLiveQuery(() => db.snapshots.where('synced').equals(0).toArray(), []) || []
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [isSyncing, setIsSyncing] = useState(false)
+
+    async function handleSync(e) {
+        e.preventDefault()
+        setIsSyncing(true)
+        try {
+            const authData = await pb.collection('users').authWithPassword(username, password)
+            console.log("authenticated successfully")
+
+            const patientsToSync = await db.patients.where('synced').equals(0).toArray();
+            const snapshotsToSync = await db.snapshots.where('synced').equals(0).toArray(); 
+            console.log(`Found ${patientsToSync.length} patients and ${snapshotsToSync.length} snapshots to sync.`);
+
+            for (const p of patientsToSync) {
+                try {
+                    const record = await pb.collection('patients').create({
+                        name: p.name
+                    })
+                    await db.patients.update(p.id, { pbId: record.id, synced: 1})
+                    console.log("Successfully uploaded unsynced patients.")
+                } catch (error) {
+                    console.error(`Failed to upload patient ${p.id}`, error)
+                }
+            }
+
+            const allPatients = await db.patients.toArray()
+            const idMap = {}
+            allPatients.forEach(p => {
+                idMap[p.id] = p.pbId
+            })
+
+            for (const s of snapshotsToSync) {
+                try {
+                    const serverPatientId = idMap[s.patientId]
+    
+                    const record = await pb.collection('snapshots').create({
+                        patientId: serverPatientId,
+                        timestamp: s.timestamp,
+                        risk: s.risk,
+                        profile: s.profile
+                    })
+    
+                    await db.snapshots.update(s.id, {pbId: record.id, synced: 1})
+                    console.log("Successfully uploaded unsynced snapshots.")
+                } catch (error) {
+                    console.error(`Failed to upload snapshot ${s.id}`, error)
+                }
+            }
+
+        } catch (error) {
+            alert("Login failed. Please check your credentials.")
+        }
+        setIsSyncing(false)
+    }
+
     return (
-        <h1>Sync Data</h1>
+        <>
+        <div className="page-container1">
+            <div className="card">
+                <div className="card-header">Sync Patient Data</div>
+                <div className="card-body">
+                    <form onSubmit={handleSync}>
+                        <div>
+                            <label>Username</label>
+                            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <button type="submit" disabled={isSyncing}>{isSyncing? "Syincing..." : "Sync Data"}</button>
+                    </form>
+                </div>
+            </div>
+            <div>{unsyncedPatients.length === 0 && unsyncedSnapshots.length === 0 ? 
+                "All records synced" : 
+                `Pending records to sync: ${unsyncedPatients.length} patients and ${unsyncedSnapshots.length} snapshots`}</div>
+        </div>
+        </>
     )
 }
 
