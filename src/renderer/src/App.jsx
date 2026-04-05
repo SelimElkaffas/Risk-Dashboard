02b3e23e9e734d3fb7a71539b2cafa5cdeef024c
Selim Elkaffas <selimkaffas@gmail.com>
2026-04-05 15:10:02 +0200

    Added routing to sync settings

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -2,6 +2,7 @@ import { HashRouter, Routes, Route, NavLink } from "react-router-dom"
 import Calculator from "./Calculator"
 import PatientManager from "./PatientManager"
 import PatientProfile from "./PatientProfile"
+import SyncSettings from "./SyncSettings"
 import './styles/App.css' 
 
 function App() {
@@ -10,11 +11,13 @@ function App() {
             <nav>
                 <NavLink to="/">Calculator</NavLink>
                 <NavLink to="/patients">Patient Manager</NavLink>
+                <NavLink to="/sync">Sync Data</NavLink>
             </nav>
             <Routes>
                 <Route path="/" element={<Calculator />} />
                 <Route path="/patients" element={<PatientManager />} />
                 <Route path="/patients/:id" element={<PatientProfile />}></Route>
+                <Route path="/sync" element={<SyncSettings />}></Route>
             </Routes>
         </HashRouter>
     );
