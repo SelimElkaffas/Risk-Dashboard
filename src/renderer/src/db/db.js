e37bd7259a863728ccf5aad64bfe27bad5865c0a
Selim Elkaffas <selimkaffas@gmail.com>
2026-04-05 15:58:19 +0200

    Added fielts for pocketbase integration

--- src/renderer/src/db/db.js
+++ src/renderer/src/db/db.js
@@ -5,8 +5,4 @@ db.version(1).stores({
     patients: "++id, name, pbId, synced",
     snapshots: "++id, patientId, timestamp, pbId, synced"
 })
-
-// // Delete any associated snapshots when a patient is deleted
-// db.patients.hook('deleting', function (primaryKey, obj, transaction) {
-//     transaction.table('snapshots').where('patientId').equals(Number(primaryKey)).delete()
-// })
+ 
